eebc8e220bedc8106897428e09dd6c938cf1e311
Alejandro <soaresalejandro@outlook.com>
2022-01-17 21:31:27 -0300

    refact: signals in vendors

--- dm/dm/vendors/admin.py
+++ dm/dm/vendors/admin.py
@@ -2,16 +2,20 @@ from django.contrib import admin
 
 from .models import Vendor, LeadTimeVendors
 
+
 class VendorAdmin(admin.ModelAdmin):
-	list_display = ('name',)
-	fields = ('name','contactInformation','averageLeadTime','maxLeadTime','minLeadTime')
-	readonly_fields = ('averageLeadTime','maxLeadTime','minLeadTime')
+    list_display = ('name',)
+    fields = ('name', 'contact', 'average_lead_time',
+              'max_lead_time', 'min_lead_time')
+    readonly_fields = ('average_lead_time', 'max_lead_time', 'min_lead_time')
+
 
 admin.site.register(Vendor, VendorAdmin)
 
 
 class LeadTimeVendorsAdmin(admin.ModelAdmin):
-	list_display = ('vendor',)
-	fields = ('vendor','leadTime')
+    list_display = ('vendor',)
+    fields = ('vendor', 'lead_time')
+
 
 admin.site.register(LeadTimeVendors, LeadTimeVendorsAdmin)
--- dm/dm/vendors/models.py
+++ dm/dm/vendors/models.py
@@ -3,74 +3,119 @@ from django.db.models import Max, Avg, Min
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
+
 class Vendor(models.Model):
-	name = models.CharField(verbose_name="Nombre", max_length=50)
-	contactInformation =  models.TextField(verbose_name="Informacion de contacto")
-	averageLeadTime = models.DecimalField(
-		verbose_name="Tiempo de entrega promedio (en dias)", 
-		max_digits=4, decimal_places=1, 
-		null=True
-		)
-	maxLeadTime = models.DecimalField(verbose_name="Tiempo de entrega maximo  (en dias)", max_digits=4, decimal_places=1, null=True)
-	minLeadTime = models.DecimalField(verbose_name="Tiempo de entrega minimo  (en dias)", max_digits=4, decimal_places=1, null=True)
+    """Vendor Model"""
 
-	class Meta:
-		verbose_name = "Proveedor"
-		verbose_name_plural = "Proveedores"
+    name = models.CharField("Nombre", max_length=50)
+    contact = models.TextField("Informacion de contacto")
+    average_lead_time = models.DecimalField(
+        "Tiempo de entrega promedio (en dias)",
+        max_digits=4,
+        decimal_places=1,
+        null=True
+    )
+    max_lead_time = models.DecimalField(
+        "Tiempo de entrega maximo  (en dias)",
+        max_digits=4,
+        decimal_places=1,
+        null=True
+    )
+    min_lead_time = models.DecimalField(
+        "Tiempo de entrega minimo  (en dias)",
+        max_digits=4,
+        decimal_places=1,
+        null=True
+    )
 
+    class Meta:
+        verbose_name = "Proveedor"
+        verbose_name_plural = "Proveedores"
 
-	def __str__(self):
-		return self.name
+    def set_lead_time(self, lead_time):
+        """Set the fields:
+            -average_lead_time
+            -max_lead_time
+            -min_lead_time
+        """
+
+        if self.average_lead_time == None:
+            self.average_lead_time = lead_time
+
+        if self.max_lead_time == None:
+            self.max_lead_time = lead_time
+
+        if self.min_lead_time == None:
+            self.min_lead_time = lead_time
+
+        self.save()
+
+    def update_fields(self, dic_fields):
+        """Update fields:
+            -average_lead_time
+            -max_lead_time
+            -min_lead_time
+        """
+
+        self.average_lead_time = dic_fields['average']
+        self.max_lead_time = dic_fields['max']
+        self.min_lead_time = dic_fields['min']
+        self.save()
+
+    def __str__(self):
+        return self.name
 
 
 class LeadTimeVendors(models.Model):
-	leadTime = models.DecimalField(verbose_name="Tiempo de entrega (en dias)", max_digits=4, decimal_places=1)
-	vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    lead_time = models.DecimalField(
+        "Tiempo de entrega (en dias)",
+        max_digits=4,
+        decimal_places=1
+    )
+    vendor = models.ForeignKey(
+        Vendor,
+        on_delete=models.CASCADE,
+        related_name='lead_times'
+    )
 
-	class Meta:
-		verbose_name = "Tiempo de entrega"
-		verbose_name_plural = "Tiempos de entrega"
+    class Meta:
+        verbose_name = "Tiempo de entrega"
+        verbose_name_plural = "Tiempos de entrega"
 
+    @property
+    def Name(self):
+        return '{}'.format(self.vendor.name)
 
-	@property
-	def Name(self):
-		return '{}'.format(self.vendor.name)
-	
-
-	def __str__(self):
-		return self.Name
+    def __str__(self):
+        return self.Name
 
 
 @receiver(pre_save, sender=Vendor)
-def PreSaveVendor(sender, instance, **kwargs):
-	instance.name = instance.name.upper()
+def presave_vendor(sender, instance, **kwargs):
+    """Pre Save Vendor"""
+
+    instance.name = instance.name.upper()
 
 
 @receiver(pre_save, sender=LeadTimeVendors)
-def PreSaveLeadTimeVendors(sender, instance, **kwargs):
-	if instance.vendor.averageLeadTime == None:
-		instance.vendor.averageLeadTime = instance.leadTime
-	
-	if instance.vendor.maxLeadTime == None:
-		instance.vendor.maxLeadTime = instance.leadTime
+def presave_LeadTimeVendors(sender, instance, **kwargs):
+    """Pre save LeadTimeVendors"""
 
-	if instance.vendor.minLeadTime == None:
-		instance.vendor.minLeadTime = instance.leadTime
-	
-	instance.vendor.save()
+    instance.vendor.set_lead_time(instance.lead_time)
 
 
 @receiver(post_save, sender=LeadTimeVendors)
-def PostSaveLeadTimeVendors(sender, instance, **kwargs):
-	
-	dicQuery = LeadTimeVendors.objects.filter(vendor__id=instance.vendor.id).aggregate(Avg('leadTime'))
-	instance.vendor.averageLeadTime = dicQuery['leadTime__avg']
+def postsave_LeadTimeVendors(sender, instance, **kwargs):
+    """Post Save LeadTimeVendors"""
 
-	dicQuery = LeadTimeVendors.objects.filter(vendor__id=instance.vendor.id).aggregate(Max('leadTime'))
-	instance.vendor.maxLeadTime = dicQuery['leadTime__max']
+    lead_times = sender.objects.filter(
+        vendor__id=instance.vendor.id
+    )
 
-	dicQuery = LeadTimeVendors.objects.filter(vendor__id=instance.vendor.id).aggregate(Min('leadTime'))
-	instance.vendor.minLeadTime = dicQuery['leadTime__min']
+    query = lead_times.aggregate(
+        average=Avg('lead_time'),
+        max=Max('lead_time'),
+        min=Min('lead_time')
+    )
 
-	instance.vendor.save()
-
+    instance.vendor.update_fields(query)

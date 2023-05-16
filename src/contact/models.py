fa763a5a46e9cbfc9843b99c4aabd9abafba00ee
alejandrosoares <soaresalejandro@outlook.com>
2023-05-16 11:33:46 +0200

    style: format models file

--- src/contact/models.py
+++ src/contact/models.py
@@ -50,7 +50,7 @@ class ContactInformation(models.Model):
         validators=[phone_validator])
     whatsapp = models.BooleanField("Do you have whatsapp?", default=False)
     address = models.CharField(max_length=100)
-    
+
     class Meta:
         verbose_name = "Contact Information"
         verbose_name_plural = "Contact Information"
